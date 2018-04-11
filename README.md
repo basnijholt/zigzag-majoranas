aba78b2103233486ab7ddb9413e29a939f1f83d1
Tom Laeven <tommy997865@msn.com>
2018-04-11 12:38:45 +0200

    modified make_skew_symmetric so it works

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Majoranas in a two dimensional system
-The current goal is to be able to reproduce the results obtained in the [paper]() by Pientka
+The current goal is to be able to reproduce the results obtained in the [paper](https://journals.aps.org/prx/abstract/10.1103/PhysRevX.7.021032) by Pientka
--- symmetry.py
+++ symmetry.py
@@ -190,11 +190,7 @@ def is_antisymmetric(ham):
 
 def make_skew_symmetric(ham):
     """
-    From Bas Nijholt's code:
-    Makes a skew symmetric matrix by a matrix multiplication of a unitary
-    matrix U. This unitary matrix is taken from the Topology MOOC 0D, but
-    that is in a different basis. To get to the right basis one multiplies
-    by [[np.eye(2), 0], [0, sigma_y]].
+    Turns hamiltonian in arbitrary basis into skew symmetric matrix
 
     Parameters:
     -----------
@@ -206,15 +202,10 @@ def make_skew_symmetric(ham):
     skew_ham : numpy.ndarray
         Skew symmetrized Hamiltonian
     """
-    W = ham.shape[0] // 4
-    I = np.eye(2, dtype=complex)
-    sigma_y = np.array([[0, 1j], [-1j, 0]], dtype=complex)
-    U_1 = phs_symmetrization(I, phs(I))[0]
-    U_2 = np.bmat([[I, 0 * I], [0 * I, sigma_y]])
-    U = U_1 @ U_2
-    U = np.kron(np.eye(W, dtype=complex), U)
-    skew_ham = U @ ham @ U.H
 
+    I = np.eye(ham.shape[0], dtype=complex)
+    U = phs_symmetrization(I, phs(I))[0]
+    skew_ham =  U.T.conj() @ ham @ U
     assert is_antisymmetric(skew_ham)
 
     return skew_ham
