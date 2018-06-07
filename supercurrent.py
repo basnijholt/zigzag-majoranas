4fafd745a454b9c018b81fb9fcc78228afe398c2
Tom Laeven <tommy997865@msn.com>
2018-06-07 14:22:06 +0200

    Add kpm supercurrent method

--- supercurrent.py
+++ supercurrent.py
@@ -1,6 +1,186 @@
 import numpy as np
 import kwant
+import sns_system
+import spectrum
+import numbers
+from functools import partial
 
+sigz = kwant.continuum.discretizer.ta.array([[1,0,0,0],
+                                             [0,1,0,0],
+                                             [0,0,-1,0],
+                                             [0,0,0,-1]])
+
+def fermi_dirac(e, params):
+            if params['T']>0:
+                beta = 1/(params['k']*params['T'])
+                return np.exp(-beta*e)/(1+np.exp(-beta*e))
+            else:
+                return np.double(e<=0)
+            
+def get_cut_sites_and_indices(syst, cut_tag, direction):
+    l_cut = []
+    r_cut = []
+    cut_indices = []
+    
+    for site_idx, site in enumerate(syst.sites):
+        if site.tag[direction]==cut_tag:
+            l_cut.append(site)
+            temp = [4*site_idx, 4*site_idx+1, 4*site_idx+2, 4*site_idx+3]
+            cut_indices.append(temp)
+        if site.tag[direction]==cut_tag+1:
+            r_cut.append(site)
+            temp = [4*site_idx, 4*site_idx+1, 4*site_idx+2, 4*site_idx+3]
+            cut_indices.append(temp)
+    
+    cut_indices = np.hstack(cut_indices)
+    cut_sites   = list(zip(l_cut, r_cut))
+    
+    return (cut_indices, cut_sites)
+
+def ensure_rng(rng=None):
+    """Turn rng into a random number generator instance
+    If rng is None, return the RandomState instance used by np.random.
+    If rng is an integer, return a new RandomState instance seeded with rng.
+    If rng is already a RandomState instance, return it.
+    Otherwise raise ValueError.
+    """
+    if rng is None:
+        return np.random.mtrand._rand
+    if isinstance(rng, numbers.Integral):
+        return np.random.RandomState(rng)
+    if all(hasattr(rng, attr) for attr in ('random_sample', 'randn',
+                                           'randint', 'choice')):
+        return rng
+    raise ValueError("Expecting a seed or an object that offers the "
+                     "numpy.random.RandomState interface.")
+
+def make_projected_current(syst, params, eigvecs, cut=None):
+    """Returns a current operator `C` that projects out the Andreev vectors
+    on the right side.
+
+    The returned function `f(bra,ket)` gives the output of
+    `<bra| C (1-P) |ket>`, where `C` is the current operator,
+    and `P` a projector to the Andreev vectors.
+    """
+    kwant_operator = kwant.operator.Current(syst, sigz, where=cut)
+    kwant_operator = kwant_operator.bind(params=params)
+
+    def projected_current(bra, ket):
+        nonlocal eigvecs
+        projected_ket = eigvecs.T.conj() @ ket
+        ket = ket - eigvecs @ projected_ket
+        return kwant_operator(bra, ket)
+    return projected_current
+
+def make_local_factory(site_indices=None, eigenvecs=None, rng=0):
+            """Return a `vector_factory` that outputs local vectors.
+
+            If `sites` is provided, the local vectors belong only to
+            those sites.
+            If `eigenvecs` is provided, project out those vectors form
+            the local vectors.
+            The parameter `rng` is passed to define a seed for finding the
+            bounds of the spectrum. Using the same seed ensures reproducibility.
+            """
+            idx = 0
+            rng = ensure_rng(rng)
+            
+            def vector_factory(n):
+                nonlocal idx, rng, eigenvecs, site_indices
+                
+                if site_indices is None:
+                    site_indices = np.arange(n)
+                if idx == -1:
+                    idx += 1
+                    return np.exp(rng.rand(n) * 2j * np.pi)
+                else:
+                    vec = np.zeros(n, dtype=complex)
+                    vec[site_indices[idx]] = 1
+
+                    if eigenvecs is not None:
+                        vec = vec - eigenvecs @ eigenvecs[site_indices[idx],:].conj()
+                    idx += 1
+                    return vec
+            return vector_factory
+
+    
+"""
+1. Make system
+2. Make cut(list of sites, and list of site index)
+3. Calculate exact spectrum for k eigenvalues/vectors
+4. Calculate current from exact spectrum
+    i. Create current operator, bind parameters
+    ii. Apply current operator for each eigenstate, apply fermi-dirac
+5. Calculate current from kpm part
+          i. Create current operator w/ projected out states
+         ii. Create eigenvector factory 
+        iii. Loop over chunks of the eigenvectors:
+             iv. Create SpectralDensity object
+              v. Add moments up to correct resolution
+             vi. Integrate over spectral density
+6. Add both exact and kpm current contributions and sum over cut
+"""
+def current_kpm_exact(syst_pars, params, k, energy_resolution, cut_tag=0, direction=0, chunk_size=None):
+    I = 0
+    
+# 1. Make system
+    params.update(dict(**sns_system.constants))
+    syst = sns_system.make_sns_system(**syst_pars)
+    
+# 2. Make cut(list of sites, and list of site index)
+    (cut_indices, cut_sites) = get_cut_sites_and_indices(syst, cut_tag, direction)
+
+# 3. Calculate exact spectrum for k eigenvalues/vectors
+    ham = syst.hamiltonian_submatrix(params=params, sparse=True)
+
+    (en, evs) = spectrum.sparse_diag(ham, k=k, sigma=0)
+    
+# 4. Calculate current from exact spectrum
+#  i. Create current operator, bind parameters
+    exact_current_operator = kwant.operator.Current(syst, 
+                                                    onsite=sigz,
+                                                    where=cut_sites
+                                                   ).bind(params=params)
+
+    _fermi_dirac = partial(fermi_dirac, params=params)
+# ii. Apply current operator for each eigenstate, apply fermi-dirac
+    for (e, ev) in zip(en, evs.T):        
+        I += _fermi_dirac(e.real) * exact_current_operator(ev)
+    
+# 5. Calculate current from kpm part
+#  i. Create current operator w/ projected out states
+    kpm_current_operator = make_projected_current(syst, params, evs, cut=cut_sites)
+
+# ii. Create eigenvector factory 
+    factory = make_local_factory(site_indices=cut_indices)
+    
+#iii. Loop over chunks of the eigenvectors:
+    if chunk_size==None:
+        chunk_size = len(cut_indices)
+    
+    vectors_left = len(cut_indices)
+    while vectors_left>0:
+        chunk_size = min(vectors_left, chunk_size)
+        
+#     iv. Create SpectralDensity object
+        sd = kwant.kpm.SpectralDensity(syst,
+                                       params=params,
+                                       operator=kpm_current_operator,
+                                       num_vectors=chunk_size-1,
+                                       num_moments=2,
+                                       vector_factory=factory)
+
+#      v. Add moments up to correct resolution
+        sd.add_moments(energy_resolution=energy_resolution)
+        
+#     vi. Integrate over spectral density
+        I += sd.integrate(distribution_function=_fermi_dirac)*chunk_size#len(cut_indices)
+        
+        vectors_left-=chunk_size
+    
+    return params['e']/ params['hbar'] * sum(I)
+                
+###################### BAS'S CODE ######################
 def get_cuts(syst, lat, first_slice=0, second_slice=None, direction=1):
     """Get the sites at two postions of the specified cut coordinates.
 
