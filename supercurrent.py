44895593dd42d554ddde23e3a799279f7c992137
Tom Laeven <tommy997865@msn.com>
2018-06-13 17:22:31 +0200

    Add distributed version of non-projecting kpm calculation

--- supercurrent.py
+++ supercurrent.py
@@ -253,7 +253,7 @@ def distributed_current_kpm_exact(syst_pars, params, k, energy_resolution, dview
 
     return params['e']/ params['hbar'] * sum(I)
 
-  
+
 def divide_sites_into_chunks(vector, chunk_size):
     vector_length = len(vector)
     return [vector[start:start+chunk_size] for start in range(0, vector_length, chunk_size)]
@@ -261,6 +261,7 @@ def divide_sites_into_chunks(vector, chunk_size):
 def get_spectrum():
     global evs, en
     return (en, evs)
+
 def calc_spectrum(syst_pars, params, k):
     global evs, en
     
@@ -303,168 +304,94 @@ def calc_kpm_current(chunk_indices, syst_pars, params, cut_tag, direction, energ
     sd.add_moments(energy_resolution=energy_resolution)
         
     return sd.integrate(distribution_function=_fermi_dirac)*len(chunk_indices)
-        
-###################### BAS'S CODE ######################
-def get_cuts(syst, lat, first_slice=0, second_slice=None, direction=1):
-    """Get the sites at two postions of the specified cut coordinates.
 
-    Parameters
-    ----------
-    syst : kwant.builder.FiniteSystem
-        The finilized kwant system.
-    lat : dict
-        A container that is used to store Hamiltonian parameters.
-    """
-    if second_slice is None:
-        second_slice = first_slice + 1
-    cut_1 = [lat(*tag) for tag in [s.tag for s in syst.sites()]
-             if tag[direction] == first_slice]
-    cut_2 = [lat(*tag) for tag in [s.tag for s in syst.sites()]
-             if tag[direction] == second_slice]
-    assert len(cut_1) == len(cut_2), "first_slice and second_slince use site.tag not site.pos!"
-    return cut_1, cut_2
+def calc_kpm_current_evs(evs, syst_pars, params, cut_tag, direction, energy_resolution): 
+    factory = make_ev_factory(evs)
+    
+    params.update(dict(**sns_system.constants))
+    syst = sns_system.make_sns_system(**syst_pars)
+
+    (cut_indices, cut_sites) = get_cut_sites_and_indices(syst, cut_tag, direction)
+    
+    _fermi_dirac = partial(fermi_dirac, params=params)
 
 
-def hopping_between_cuts(syst, r_cut, l_cut):
-    r_cut_sites = [syst.sites.index(site) for site in r_cut]
-    l_cut_sites = [syst.sites.index(site) for site in l_cut]
+    exact_current_operator = kwant.operator.Current(syst, 
+                                                onsite=sigz,
+                                                where=cut_sites
+                                               ).bind(params=params)
 
-    def hopping(syst, params):
-        return syst.hamiltonian_submatrix(params=params,
-                                          to_sites=l_cut_sites,
-                                          from_sites=r_cut_sites)[::2, ::2]
-    return hopping
+    sd = kwant.kpm.SpectralDensity(syst,
+                                   params=params,
+                                   operator=exact_current_operator,
+                                   num_vectors=evs.shape[1],
+                                   num_moments=2,
+                                   vector_factory=factory)
 
-def current_at_phase(syst, hopping, params, H_0_cache, phase,
-                     tol=1e-2, max_frequencies=500):
-    """Find the supercurrent at a phase using a list of Hamiltonians at
-    different imaginary energies (Matsubara frequencies). If this list
-    does not contain enough Hamiltonians to converge, it automatically
-    appends them at higher Matsubara frequencies untill the contribution
-    is lower than `tol`, however, it cannot exceed `max_frequencies`.
+#      v. Add moments up to correct resolution
+    sd.add_moments(energy_resolution=energy_resolution)
 
-    Parameters
-    ----------
-    syst : kwant.builder.FiniteSystem
-        The finilized kwant system.
-    hopping : function
-        Function that returns the hopping matrix between the two cross sections
-        of where the SelfEnergyLead is attached.
-    params : dict
-        A container that is used to store Hamiltonian parameters.
-    H_0_cache : list
-        Hamiltonians at different imaginary energies.
-    phase : float, optional
-        Phase at which the supercurrent is calculated.
-    tol : float, optional
-        Tolerance of the `current_at_phase` function.
-    max_frequencies : int, optional
-        Maximum number of Matsubara frequencies.
-
-    Returns
-    -------
-    dict
-        Dictionary with the critical phase, critical current, and `currents`
-        evaluated at `phases`."""
-    try:
-        # Make sure that there is no SC phase.
-        params['phi'] = 0
-    except KeyError:
-        pass
-
-    H12 = hopping(syst, params)
-    I = 0
-    for n in range(max_frequencies):
-        if len(H_0_cache) <= n:
-            H_0_cache.append(null_H(syst, params, n))
-        I_contrib = current_contrib_from_H_0(H_0_cache[n], H12, phase, params)
-        I += I_contrib
-        if I_contrib == 0 or tol is not None and abs(I_contrib / I) < tol:
-            return I
-    # Did not converge within tol using max_frequencies Matsubara frequencies.
-    if tol is not None:
-        return np.nan
-    # if tol is None, return the value after max_frequencies is reached.
-    else:
-        return I
-
-def null_H(syst, params, n):
-    """Return the Hamiltonian (inverse of the Green's function) of
-    the electron part at zero phase.
-
-    Parameters
-    ----------
-    syst : kwant.builder.FiniteSystem
-        The finilized kwant system.
-    params : dict
-        A container that is used to store Hamiltonian parameters.
-    n : int
-        n-th Matsubara frequency
-
-    Returns
-    -------
-    numpy.array
-        The Hamiltonian at zero energy and zero phase."""
-    en = matsubara_frequency(n, params)
-    gf = kwant.greens_function(syst, en, out_leads=[0], in_leads=[0],
-                               check_hermiticity=False, params=params)
-    return np.linalg.inv(gf.data[::2, ::2])
+    return sd.integrate(distribution_function=_fermi_dirac)*evs.shape[1]
 
 
-def matsubara_frequency(n, params):
-    """n-th fermionic Matsubara frequency at temperature T.
+def current_kpm_non_projected(syst_pars, params,
+                              k, energy_resolution, lview,
+                              cut_tag=0, direction=0):
+    lview.block = False
 
-    Parameters
-    ----------
-    n : int
-        n-th Matsubara frequency
+    params.update(dict(**sns_system.constants))
+    syst = sns_system.make_sns_system(**syst_pars)
+    
+    (cut_indices, cut_sites) = get_cut_sites_and_indices(syst, cut_tag, direction)
 
-    Returns
-    -------
-    float
-        Imaginary energy.
-    """
-    return (2*n + 1) * np.pi * params['k'] * params['T'] * 1j
+######################################################
+    #ar_current_all_kpm   = lview.map(calc_all_kpm, chunks_kpm) #non blocking
+#####################################################
+    chunks = divide_sites_into_chunks(cut_indices, chunk_size)
 
-def current_contrib_from_H_0(H_0, H12, phase, params):
-    """Uses Dyson’s equation to obtain the Hamiltonian for other
-    values of `phase` without further inversions (calling `null_H`).
+    filled_in_calc_kpm_current = partial(calc_kpm_current,
+                                         syst_pars=syst_pars,
+                                         params=params,
+                                         cut_tag=cut_tag,
+                                         direction=direction,
+                                         energy_resolution=energy_resolution,
+                                         operator_projection=operator_projection)
+    
+    ar_current_all_kpm = lview.map(filled_in_calc_kpm_current, chunks)
 
-    Parameters
-    ----------
-    H_0 : list
-        Hamiltonian at a certain imaginary energy.
-    H12 : numpy array
-        The hopping matrix between the two cross
-        sections of where the SelfEnergyLead is attached.
-    phase : float
-        Phase at which the supercurrent is calculated.
-    params : dict
-        A container that is used to store Hamiltonian parameters.
-    Returns
-    -------
-    float
-        Current contribution of `H_0`.
-    """
-    t = H12 * np.exp(1j * phase)
-    gf = gf_from_H_0(H_0, t - H12)
-    dim = t.shape[0]
-    H12G21 = t.T.conj() @ gf[dim:, :dim]
-    H21G12 = t @ gf[:dim, dim:]
-    return -4 * params['T'] * params['current_unit'] * (
-        np.trace(H21G12) - np.trace(H12G21)).imag
+################################################
+    # ar_current_ABS_exact = calc_current_ABS_exact() #Blocking
+################################################
+    ham = syst.hamiltonian_submatrix(params=params, sparse=True)
+    (en, evs) = spectrum.sparse_diag(ham, k=k, sigma=0)
 
-def gf_from_H_0(H_0, t):
-    """Returns the Green's function at a phase that is defined inside `t`.
-    See doc-string of `current_from_H_0`.
-    """
-    H = np.copy(H_0)
-    dim = t.shape[0]
-    H[:dim, dim:] -= t.T.conj()
-    H[dim:, :dim] -= t
-    return np.linalg.inv(H)
+######################################################################
+    # ar_ABS_kpm = lview.map(calc_AB_kpm, chunks_ABS) #non blocking
+#####################################################################
+    filled_in_calc_kpm_current_evs = partial(calc_kpm_current,
+                                             syst_pars=syst_pars,
+                                             params=params,
+                                             cut_tag=cut_tag,
+                                             direction=direction,
+                                             energy_resolution=energy_resolution)   
 
+    ev_chunks = tuple(a[:,i:i+2] for i in range(a.shape[1]))
+    ar_ABS_kpm = lview.map(filled_in_calc_kpm_current_evs, ev_chunks)
 
+#############################################################
+#  Calc exact current contribution
+#############################################################
+    I_AB_exact = 0
+    for (e, ev) in zip(en, evs.T):        
+        I_AB_exact += _fermi_dirac(e.real) * exact_current_operator(ev)
 
+#######################################################
+# Wait until all done
+#######################################################
+    ar_ABS_kpm.wait()
+    ar_current_all_kpm.wait() 
 
+    I_AB_exact = ar_ABS_kpm.get()
+    I_all_kpm  = ar_current_all_kpm.get() 
 
+    return params['e']/ params['hbar'] * sum(I_AB_exact + (I_all_kpm - I_AB_kpm))
