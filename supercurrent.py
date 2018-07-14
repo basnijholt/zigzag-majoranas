d122c90777109207688ca71366aa5e7d83b8f77d
Tom Laeven <tlaeven@hotmail.com>
2018-07-14 11:46:47 +0200

    Add option for zeeman in sc for current

--- supercurrent.py
+++ supercurrent.py
@@ -40,8 +40,8 @@ def wrapped_current_k(k, syst_pars, params, cut_tag=0, direction=0):
 
     return sum(I)*params['e']/params['hbar']
 
-def wrapped_current(syst_pars, params, tol=0.01, max_iterations=1e3):
-    syst_wrapped = sns_system.make_wrapped_system(**syst_pars)
+def wrapped_current(syst_pars, params, tol=0.01, max_iterations=1e3, , transverse_soi=True, zeeman_in_superconductor=False):
+    syst_wrapped = sns_system.make_wrapped_system(**syst_pars, transverse_soi, zeeman_in_superconductor)
     cut_tag=0
     direction=0
 
